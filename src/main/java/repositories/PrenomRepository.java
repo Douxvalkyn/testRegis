61b4a93be9d6d8efa5de846c1f72e87ddd792d9c
regis relland <douxvalkyn@gmail.com>
2022-02-05 19:39:13 +0100

    premier essai WS prenom

---
+++ src/main/java/repositories/PrenomRepository.java
@@ -0,0 +1,10 @@
+package repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import entities.Prenom;
+
+public interface PrenomRepository  extends CrudRepository<Prenom, Integer>{
+
+	
+}
